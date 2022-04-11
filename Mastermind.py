75c2a5c6150b3860f40539ecb0b186a56e27b8b7
Pierre RATCLIFFE <pierre.ratcliffe@ens.uvsq.fr>
2022-04-11 17:44:38 +0200

    couleur cercle rotation avant et arriere fini

--- Mastermind.py
+++ Mastermind.py
@@ -4,7 +4,7 @@
 # Yassine BAGHDAD-BEY
 # Sephora TAHA
 # Nique PRYSCIA
-# https://github.com/uvsq22008849/projet_mastermind.git
+# https://github.com/uvsq22008849/projet_tas_de_sable.git
 #########################################
 
 
@@ -17,11 +17,68 @@ import random
 
 racine = tk.Tk()
 
-HEIGHT = 500
+HEIGHT = 540
 WIDTH = 490
 
+rows, cols = (10, 4)
+
+liste_cellule = [[0 for i in range(cols)] for j in range(rows)]
+liste_cercle = [[0 for i in range(cols)] for j in range(rows)]
+liste_cellule_code_secret = [0 for i in range(cols)]
+liste_cercle_code_secret = [0 for i in range(cols)]
+#print(liste_cercle_code_secret)
+#print(liste_cellule)
 
 ######################### Fonctions ###############################
+def couleur_cellule():
+    for i in range(cols):
+        if liste_cellule_code_secret[i] == 1:
+            canvas.itemconfigure(liste_cercle_code_secret[i], fill = "blue")
+        if liste_cellule_code_secret[i] == 2:
+            canvas.itemconfigure(liste_cercle_code_secret[i], fill = "red")
+        if liste_cellule_code_secret[i] == 3:
+            canvas.itemconfigure(liste_cercle_code_secret[i], fill = "green")
+        if liste_cellule_code_secret[i] == 4:
+            canvas.itemconfigure(liste_cercle_code_secret[i], fill = "yellow")
+        if liste_cellule_code_secret[i] == 5:
+            canvas.itemconfigure(liste_cercle_code_secret[i], fill = "pink")
+        if liste_cellule_code_secret[i] == 6:
+            canvas.itemconfigure(liste_cercle_code_secret[i], fill = "black")
+        if liste_cellule_code_secret[i] == 7:
+            canvas.itemconfigure(liste_cercle_code_secret[i], fill = "grey")
+        if liste_cellule_code_secret[i] == 8:
+            canvas.itemconfigure(liste_cercle_code_secret[i], fill = "orange")
+
+    
+def cliqueG_code_secret(event):
+    global x0, y0, x1, y1
+    x = event.x
+    y = event.y
+    for i in range(4):
+        x0, y0, x1, y1 = canvas.coords(liste_cercle_code_secret[i])
+        if x > x0 and x < x1 and y > y0 and y < y1:
+            liste_cellule_code_secret[i] += 1
+            if liste_cellule_code_secret[i] > 8:
+                liste_cellule_code_secret[i] = 1
+    couleur_cellule()
+
+def cliqueD_code_secret(event):
+    global x0, y0, x1, y1
+    x = event.x
+    y = event.y
+    for i in range(4):
+        x0, y0, x1, y1 = canvas.coords(liste_cercle_code_secret[i])
+        if x > x0 and x < x1 and y > y0 and y < y1:
+            liste_cellule_code_secret[i] -= 1
+            if liste_cellule_code_secret[i] == 0:
+                liste_cellule_code_secret[i] = 8
+    couleur_cellule()
+            #if x > j * 100 and x < j * 100 + 100 and y > i * 100 and y < i * 100 + 100:
+            #    liste_cellule[i][j] += 1
+
+        
+
+#def code_secret():
 
 
 ######################### Pogramme ################################
@@ -42,11 +99,18 @@ for y in range(10):
 #160 250 230 320
 #90 à 150 donc ligne à y = 120
 for z in range(4):
-    canvas.create_oval((60 + (z * 100), 20, (((z + 1) * 100) + 30, 90)), outline = "black", width = 5)
+    cercle = canvas.create_oval((60 + (z * 100), 20, (((z + 1) * 100) + 30, 90)), outline = "black", width = 5)
+    liste_cercle_code_secret[z] = cercle
+#print(liste_cercle_code_secret)
 
 canvas.create_line((60, 120), (430, 120), fill="black", width=2)
 
+bouton_iteration = tk.Button(racine, text = "Prochaine étape", padx = 20, pady = 20, font =("Helvetica", "20"))
+bouton_iteration.grid(row = 1, column = 0)
+
 scroll_y.config(command = canvas.yview)
+canvas.bind("<Button-1>", cliqueG_code_secret)
+canvas.bind("<Button-2>", cliqueD_code_secret)
 
 
 racine.mainloop()
