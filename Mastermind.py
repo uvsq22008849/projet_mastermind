ec34e1ee4a0e426b3fa8e7fa911e73132ea0c19c
Pierre RATCLIFFE <pierre.ratcliffe@ens.uvsq.fr>
2022-04-20 16:59:33 +0200

    recommendation d'une iteration

--- Mastermind.py
+++ Mastermind.py
@@ -47,18 +47,17 @@ liste_cercle_indicateurs_D_bas = [[0 for i in range(2)] for j in range(rows)]
 liste_cpt_cellule = [[0 for i in range(9)] for j in range(rows)]
 liste_cpt_code = [0 for i in range(9)]
 
+#liste_recommendation = [[0 for i in range(cols)] for j in range(rows)]
+
 
 stop = 0
 cpt = 0
 #pivot = 0
-cpt0, cpt1, cpt2, cpt3, cpt4, cpt5, cpt6, cpt7, cpt8 = 0, 0, 0, 0, 0, 0, 0, 0, 0
-cpt0_cellule, cpt1_cellule, cpt2_cellule, cpt3_cellule, cpt4_cellule, cpt5_cellule, cpt6_cellule, cpt7_cellule, cpt8_cellule = 0, 0, 0, 0, 0, 0, 0, 0, 0
 tvar = tk.StringVar()
 tvar.set("Cache moi ce code ;)")
 
 ######################### Fonctions ###############################
 def couleur_cellule():
-    global cpt0, cpt1, cpt2, cpt3, cpt4, cpt5, cpt6, cpt7, cpt8
     for i in range(cols):
         if liste_cellule_code_secret[i] == 0:
             canvas.itemconfigure(liste_cercle_code_secret[i], fill = "white")
@@ -256,60 +255,9 @@ def nombre_couleur_ligne():
                 elif liste_cellule[y][i] == 8:
                     liste_cpt_cellule[y][8] += 1
     #print(liste_cpt_cellule)
-
-'''Bonne couleur bonne place --> enlever  de indicateur gauche'''
-"""def indicateurs_G():
-    cpt2 = 0
-    liste_verifiacation_couleur_differente = [0 for i in range(cols)]
-    liste_cellule_deja_compté = [[0 for i in range(cols)] for j in range(rows)]
-    liste_transitoire = [[0 for i in range(cols)] for j in range(rows)]
-    liste_transitoire = list(liste_cellule)
-    #print(liste_transitoire)
-    for y in range(rows):
-        for i in range(cols):
-            if liste_DEFINITF_cellule_code_secret[i] == liste_cellule[y][i]:
-                       liste_cellule_deja_compté[y][i] += 1
-                       #print(liste_cellule_deja_compté)
-                       #print(liste_cellule_deja_compté)
-            #           cpt2 += 1
-            #if liste_cellule_deja_compté[i] != 4:
-            #    if liste_DEFINITF_cellule_code_secret[i] == liste_cellule_deja_compté
-            for k in range(cols):
-                #if liste_DEFINITF_cellule_code_secret[i] == liste_cellule[y][i]:
-                #    liste_transitoire[y][i] = 9
-                if y == (cpt - 2) and liste_DEFINITF_cellule_code_secret[i] == liste_cellule[y][k] and liste_cellule_deja_compté[y][k] < 1:# and liste_DEFINITF_cellule_code_secret[i] != liste_cellule[y][i]:# and k != liste_cellule_deja_compté[i]:
-                    #if liste_cellule_deja_compté[i] != 4:
-                        '''if liste_DEFINITF_cellule_code_secret[i] == liste_cellule[y][i]:
-                        i_fixe = i'''
-                        #if liste_DEFINITF_cellule_code_secret[i] == liste_cellule[y][i]:
-                        #    liste_cellule_deja_compté[i] = liste_cellule[y][i]
-                        liste_cellule_deja_compté[y][k] += 1
-                        if liste_DEFINITF_cellule_code_secret[i] == liste_cellule[y][i]:
-                            break
-                        if liste_indicateurs_G_haut[y][0] == 0:
-                            canvas.itemconfigure(liste_cercle_indicateurs_G_haut[y][0], fill = "red", outline = "red")
-                            liste_indicateurs_G_haut[y][0] += 1
-                            #t = liste_DEFINITF_cellule_code_secret[i]
-                            #liste_verifiacation_couleur_differente[0] = t
-                            break
-                        elif liste_indicateurs_G_haut[y][0] != 0 and liste_indicateurs_G_haut[y][1] == 0:
-                            canvas.itemconfigure(liste_cercle_indicateurs_G_haut[y][1], fill = "red", outline = "red")
-                            liste_indicateurs_G_haut[y][1] += 1
-                            #t = liste_DEFINITF_cellule_code_secret[i]
-                            #liste_verifiacation_couleur_differente[1] = t
-                            break
-                        elif liste_indicateurs_G_haut[y][0] != 0 and liste_indicateurs_G_haut[y][1] != 0 and liste_indicateurs_G_bas[y][0] == 0:
-                            canvas.itemconfigure(liste_cercle_indicateurs_G_bas[y][0], fill = "red", outline = "red")
-                            liste_indicateurs_G_bas[y][0] += 1
-                            #t = liste_DEFINITF_cellule_code_secret[i]
-                            #liste_verifiacation_couleur_differente[2] = t
-                            break
-                        elif liste_indicateurs_G_haut[y][0] != 0 and liste_indicateurs_G_haut[y][1] != 0 and liste_indicateurs_G_bas[y][0] != 0:
-                            canvas.itemconfigure(liste_cercle_indicateurs_G_bas[y][1], fill = "red", outline = "red")
-                            liste_indicateurs_G_bas[y][1] += 1"""
               
 def indicateurs_G():
-    liste_position_indice = [4 for i in range(cols)]
+    #liste_position_indice = [4 for i in range(cols)]
     liste_nbre_indicateur_G = [0 for i in range(rows)]
     for y in range(rows):
         if liste_indicateurs_D_haut[y][0] == 1:
@@ -326,55 +274,58 @@ def indicateurs_G():
                 liste_nbre_indicateur_G[y] += liste_cpt_cellule[y][i + 1]
             if liste_cpt_cellule[y][i + 1] > liste_cpt_code[i + 1]:
                 liste_nbre_indicateur_G[y] += liste_cpt_code[i + 1]
-            '''if i < cols:
-                if liste_DEFINITF_cellule_code_secret[i] == liste_cellule[y][i]:
-                    liste_nbre_indicateur_G[y] -= 1'''
-            """if liste_indicateurs_D_haut[y][0] == 1:
-                liste_nbre_indicateur_G[y] -= 1
-            if liste_indicateurs_D_haut[y][1] == 1:
-                liste_nbre_indicateur_G[y] -= 1
-            if liste_indicateurs_D_bas[y][0] == 1:
-                liste_nbre_indicateur_G[y] -= 1
-            if liste_indicateurs_D_bas[y][1] == 1:
-                liste_nbre_indicateur_G[y] -= 1"""
             if liste_nbre_indicateur_G[y] == 1:
                 canvas.itemconfigure(liste_cercle_indicateurs_G_haut[y][0], fill = "black", outline = "black")
+                liste_indicateurs_G_haut[y][0] = 1
             if liste_nbre_indicateur_G[y] == 2:
                 canvas.itemconfigure(liste_cercle_indicateurs_G_haut[y][0], fill = "black", outline = "black")
                 canvas.itemconfigure(liste_cercle_indicateurs_G_haut[y][1], fill = "black", outline = "black")
+                liste_indicateurs_G_haut[y][0] = 1
+                liste_indicateurs_G_haut[y][1] = 1
             if liste_nbre_indicateur_G[y] == 3:
                 canvas.itemconfigure(liste_cercle_indicateurs_G_haut[y][0], fill = "black", outline = "black")
                 canvas.itemconfigure(liste_cercle_indicateurs_G_haut[y][1], fill = "black", outline = "black")
                 canvas.itemconfigure(liste_cercle_indicateurs_G_bas[y][0], fill = "black", outline = "black")
+                liste_indicateurs_G_haut[y][0] = 1
+                liste_indicateurs_G_haut[y][1] = 1
+                liste_indicateurs_G_bas[y][0] = 1
             if liste_nbre_indicateur_G[y] == 4:
                 canvas.itemconfigure(liste_cercle_indicateurs_G_haut[y][0], fill = "black", outline = "black")
                 canvas.itemconfigure(liste_cercle_indicateurs_G_haut[y][1], fill = "black", outline = "black")
                 canvas.itemconfigure(liste_cercle_indicateurs_G_bas[y][0], fill = "black", outline = "black")
                 canvas.itemconfigure(liste_cercle_indicateurs_G_bas[y][1], fill = "black", outline = "black") 
+                liste_indicateurs_G_haut[y][0] = 1
+                liste_indicateurs_G_haut[y][1] = 1
+                liste_indicateurs_G_bas[y][0] = 1
+                liste_indicateurs_G_bas[y][1] = 1
     #print(liste_nbre_indicateur_G)
     #nbre_indicateur_G = 0
-        #print(nbre_indicateur_G)
-    '''for i in range(cols):
-        for k in range(cols):
-            if liste_DEFINITF_cellule_code_secret[0] == liste_cellule[y][k]:
-                liste_position_indice[0] = k
-                break
-            if liste_DEFINITF_cellule_code_secret[1] == liste_cellule[y][k] and liste_position_indice[0] == 4:# and k != liste_position_indice[0]:
-                liste_position_indice[1] = k
-                break
-            if liste_DEFINITF_cellule_code_secret[1] == liste_cellule[y][k] and liste_position_indice[0] != 4 and k != liste_position_indice[0]:
-                liste_position_indice[1] = k
-                break '''
-                        
+
+def recommendation():
+    couleur_aleatoire = random.randint(1, 8)
+    for y in range(rows):
+        for i in range(cols):
+            if y == (cpt - 1):
+                if y == 0:
+                    liste_cellule[y][i] = couleur_aleatoire
+                elif y == 1 and liste_indicateurs_D_haut[y - 1][0] != 1 and liste_indicateurs_G_haut[y - 1][0] != 1:
+                    liste_cellule[y][i] = couleur_aleatoire
+                elif liste_indicateurs_G_haut[y - 1][0] == 1:
+                    liste_transitoire = list(liste_cellule[y - 1])
+                    random.shuffle(liste_transitoire)
+                    liste_cellule[y] = list(liste_transitoire)
+    couleur_cellule()
+
+#def retour_en_arriere():
 
 
 ######################### Pogramme ################################
 
 scroll_y = tk.Scrollbar(racine, orient = 'vertical')
-scroll_y.grid(row = 0, column = 1, sticky = 'ns')
+scroll_y.grid(row = 0, column = 2, sticky = 'ns')
 
 canvas = tk.Canvas(racine, bg = "white", height=HEIGHT, width=WIDTH, scrollregion = (0, 0, 490, 1140), yscrollcommand = scroll_y.set)
-canvas.grid(row = 0, column = 0)
+canvas.grid(row = 0, column = 0, columnspan = 2)
 
 #print(tkFont.families())
 
@@ -394,26 +345,6 @@ for y in range(10):
 for z in range(4):
     cercle_code = canvas.create_oval((60 + (z * 100), 20, (((z + 1) * 100) + 30, 90)), outline = "black", width = 5)
     liste_cercle_code_secret[z] = cercle_code
-#print(liste_cercle_code_secret)
-
-#Commencer à x=17 du bord avec des cercle de d=10 et x=6 d'ecart entre eux
-#Commencer à y=172 avec meme d et meme écart
-'''for y in range(10):
-    for t in range(2):
-        for i in range(2):
-            cercle_indicateurs_G = canvas.create_oval((17 + (i * 16), 172 + (t * 16) + (y * 100)), (27 + (i * 16), 182 + (t * 16) + (y * 100)), outline = "white", width = 5, fill = 'white')
-            liste_cercle_indicateurs_G[y][i] = cercle_indicateurs_G
-"""Indicateurs gauche, cercles de diamètre 10 séparés de x et y = 6 pxl"""
-#print(canvas.coords(liste_cercle_indicateurs[0][0]))
-#print(canvas.coords(liste_cercle[0][0]))
-
-for y in range(10):
-    for t in range(2):
-        for i in range(2):
-            cercle_indicateurs_D = canvas.create_oval((447 + (i * 16), 172 + (t * 16) + (y * 100)), (457 + (i * 16), 182 + (t * 16) + (y * 100)), outline = "red", width = 5, fill = 'white')
-            liste_cercle_indicateurs_D[y][i] = cercle_indicateurs_D'''
-
-#print(cercle_indicateurs_D)
 
 for y in range(10):
     for i in range(2):
@@ -440,8 +371,15 @@ for y in range(10):
 ligne_separation = canvas.create_line((60, 120), (430, 120), fill="black", width=2)
 
 
-bouton_iteration = tk.Button(racine, textvariable = tvar, command = cacher_code_secret, padx =16, pady =18, bd ='5', bg ='blue', font =("Optima", "23"))
+bouton_iteration = tk.Button(racine, textvariable = tvar, command = cacher_code_secret, padx =16, pady =18, bd ='5', bg ='blue', font =("Optima", "23"), width = 14)
 bouton_iteration.grid(row = 1, column = 0)
+
+bouton_recommendation = tk.Button(racine, text = "Besoin d'aide ?", command = recommendation, padx =16, pady =18, bd ='5', bg ='blue', font =("Optima", "23"))
+bouton_recommendation.grid(row = 2, column = 0)
+
+bouton_retour_en_arriere = tk.Button(racine, text = "Retour en arrière", padx =16, pady =18, bd ='5', bg ='blue', font =("Optima", "23"), width = 10)
+bouton_retour_en_arriere.grid(row = 1, column = 1)
+
 
 scroll_y.config(command = canvas.yview)
 canvas.bind("<Button-1>", cliqueG_code_secret)
